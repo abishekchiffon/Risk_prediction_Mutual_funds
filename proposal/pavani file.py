ea215c5d235231e9bafa92ad5e68eeefba9cfe30
pavanichella12 <pavani.chella@gwu.edu>
2023-12-11 14:56:12 -0500

    made changes

--- proposal/pavani file.py
+++ proposal/pavani file.py
@@ -71,3 +71,52 @@ print(top10_funds_by_risk)
 print("\nMost prevalent fund categories among 'Gold' rated funds:")
 print(most_prevalent_categories)
 # %%
+#10. For funds with a high risk rating, what investment strategies are commonly employed?
+# Assuming df is your DataFrame
+# Replace 'risk_rating' and 'investment_strategy' with the provided column names
+risk_rating_column = 'risk_rating'
+investment_strategy_column = 'investment_strategy'
+
+# Filter funds with a high risk rating (assuming 'High' as the risk rating)
+high_risk_funds = df[df[risk_rating_column] == 'High']
+
+# Display the distribution of investment strategies for funds with a high risk rating
+investment_strategy_distribution = high_risk_funds[investment_strategy_column].value_counts()
+
+print("Commonly employed investment strategies for funds with a high risk rating:")
+print(investment_strategy_distribution)
+
+# %%
+#11. Which fund category has shown the most improvement in average return over the past
+#three years?
+import pandas as pd
+
+# Assuming df is your DataFrame
+# Replace 'category', 'fund_trailing_return_3years', 'fund_return_2019', 'fund_return_2018', and 'fund_return_2017' with the actual column names
+category_column = 'category'
+returns_columns = [
+    'fund_trailing_return_3years',
+    'fund_return_2019',
+    'fund_return_2018',
+    'fund_return_2017'
+]
+
+# Filter relevant columns
+returns_data = df[[category_column] + returns_columns]
+
+# Calculate average returns for each fund category
+average_returns = returns_data.groupby(category_column).mean()
+
+# Calculate improvement over the past three years
+improvement_over_three_years = (
+    average_returns['fund_trailing_return_3years'] -
+    average_returns[['fund_return_2019', 'fund_return_2018', 'fund_return_2017']].mean(axis=1)
+)
+
+# Identify the fund category with the most improvement
+most_improved_category = improvement_over_three_years.idxmax()
+
+print("Fund category with the most improvement in average return over the past three years:")
+print(most_improved_category)
+
+#%%
