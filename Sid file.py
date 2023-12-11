c7d21405434de792153aa8ecd91ab66b89f5254e
SidNadadhur <143538274+SidNadadhur@users.noreply.github.com>
2023-12-12 01:14:04 +0530

    handling missing values

--- Sid file.py
+++ Sid file.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 11 05:36:36 2023
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import skew
+import statsmodels.api as sm
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from scipy.stats import pointbiserialr
+from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression
 
-@author: hp
-"""
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from scipy.stats import kruskal
 
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import OneHotEncoder
+from scipy.sparse import hstack
+from sklearn.linear_model import ElasticNet
